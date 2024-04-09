85b081943ada01397096ceabd4a54a43139e2ec0
Justin Lubin <jlubi333@gmail.com>
2024-04-09 13:07:25 -0700

    Do subsampling for data

--- stuff/subsample.py
+++ stuff/subsample.py
@@ -2,9 +2,14 @@
 
 import pandas as pd
 
+import shutil
+import gzip
+
 # %% Load data
 
-data = pd.read_csv("data/poas.csv")
+DATA_DIR = "data"
+
+data = pd.read_csv(f"{DATA_DIR}/poas.csv")
 
 # %% Run
 
@@ -29,55 +34,19 @@ def subsample(df, p):
 
 
 for p in PERCENTS:
-    subsampled_datas.append(
-        pd.concat(
-            [
-                subsample(test, p),
-                subsample(val, p),
-                subsample(train, p),
-            ]
-        ).reset_index(drop=True)
-    )
+    p_str = str(p).replace(".", "_")
+    filename = f"{DATA_DIR}/poas-{p_str}.csv"
 
-for df in subsampled_datas:
-    df.to_csv
+    pd.concat(
+        [
+            subsample(test, p),
+            subsample(val, p),
+            subsample(train, p),
+        ]
+    ).reset_index(
+        drop=True
+    ).to_csv(filename)
 
-
-# test_data = (
-#    data[data["partition"] != "test"]
-#    .reset_index(
-#        drop=True,
-#    )
-# )
-
-# %% Run
-
-FEATURES = LargeMoleculeDescriptors.descriptor_names()
-
-
-def aho_to_descriptor_dict(seq_aho, *, prefix):
-    seq = seq_aho.replace("-", "")
-    lmd = LargeMoleculeDescriptors.from_sequence(seq)
-    d = lmd.asdict()
-    return {f"{prefix}{k}": v for k, v in d.items() if k in FEATURES}
-
-
-def samples_to_descriptors(samples, show_progress=None):
-    descriptors = []
-    for i, row in samples.iterrows():
-        heavy = aho_to_descriptor_dict(row["fv_heavy_aho"], prefix="fv_heavy_")
-        light = aho_to_descriptor_dict(row["fv_light_aho"], prefix="fv_light_")
-        descriptors.append(heavy | light | dict(row))
-        if show_progress and i % show_progress == 0:
-            print(i / len(samples))
-    return pd.DataFrame(descriptors)
-
-
-a = samples_to_descriptors(test_data.iloc[0:5000])
-b = samples_to_descriptors(test_data.iloc[5001:10000])
-
-per_column_wd, avg_wd, total_wd, _ = get_batch_descriptors(
-    b,
-    ref_feats=a,
-    chain="fv_heavy",
-)
+    with open(filename, "rb") as f_in:
+        with gzip.open(filename + ".gz", "wb") as f_out:
+            shutil.copyfileobj(f_in, f_out)
