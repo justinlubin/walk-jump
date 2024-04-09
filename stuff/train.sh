8bab2b02cb1f80dd18f6c91c1f5cd289f9af44f0
Justin Lubin <jlubi333@gmail.com>
2024-04-09 13:25:50 -0700

    Work on train script

--- stuff/train.sh
+++ stuff/train.sh
@@ -1,1 +1,15 @@
-walkjump_train data.csv_data_path="data/poas.csv.gz"
+if [ -d "checkpoints/" ]; then
+  echo "checkpoints directory already exists"
+  exit 1
+fi
+
+DATA_DIR="data/poas-$1.csv.gz"
+
+if [ ! -f "$DATA_DIR" ]; then
+  echo "data file for percent '$1' does not exist"
+  exit 2
+fi
+
+WANDB_MODE=offline walkjump_train data.csv_data_path="$DATA_DIR"
+mv checkpoints/last.ckpt "../wj-checkpoints/data-$1-sigma-0_5"
+rm -rf checkpoints/
