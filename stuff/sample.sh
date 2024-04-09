22298d21fdbe06da5d5b4f8bdc6876b9d9aa72bb
Justin Lubin <jlubi333@gmail.com>
2024-04-09 11:27:39 -0700

    Work on some scripts

--- stuff/sample.sh
+++ stuff/sample.sh
@@ -1,2 +1,4 @@
-#!/usr/bin/env bash
-walkjump_sample "model.checkpoint_path=../wj-checkpoints/$1.ckpt" "designs.output_csv=stuff/samples/$1.csv"
+walkjump_sample \
+  "model.checkpoint_path=../wj-checkpoints/$1.ckpt" \
+  "designs.output_csv=stuff/samples/$1.csv" \
+  "designs.num_samples=$2"
--- stuff/train.sh
+++ stuff/train.sh
@@ -1,2 +1,1 @@
-#!/usr/bin/env bash
 walkjump_train data.csv_data_path="data/poas.csv.gz"
